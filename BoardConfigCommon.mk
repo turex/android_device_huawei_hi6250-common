f11c05fc6b883e152cf53590e6ca856076856497
turex <salvo95ct@gmail.com>
2021-09-24 13:31:29 +0200

    BoardConfig: Set kernel toolchain

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,6 +47,10 @@ TARGET_KERNEL_SOURCE := kernel/huawei/hi6250
 TARGET_KERNEL_CONFIG := merge_hi6250_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
+#Toolchain
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-7.5/bin
+KERNEL_TOOLCHAIN_PREFIX := aarch64-elf-
+
 # Properties
 BOARD_PROPERTY_OVERRIDES_SPLIT_ENABLED := true
 
